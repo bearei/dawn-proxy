77c4dd5e5a1c54e77780a2c9bc5a777639976205
Dawn <dawnsaq@gmail.com>
2019-03-11 08:51:05 -0700

    updated url endpoints

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+require('newrelic');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const proxy = require('http-proxy-middleware');
+
+// start redis server in CLI: redis-server
+// var redis = require('redis');
+// var client = redis.createClient();
+// var client = redis.createClient(port);
+// client.on('connect', function() {
+//   console.log('Redis client connected');
+// });
+// client.on('error', function(err) {
+//   console.log('Something went wrong ' + err);
+// });
 
 app.use(morgan('dev'));
 app.use('/:itemId', express.static(path.join(__dirname, 'public')));
@@ -16,8 +28,26 @@ app.use('/:itemId', express.static(path.join(__dirname, 'public')));
 //   next();
 // });
 
+// client.on('error', function(err) {
+//   console.log('error' + err);
+// });
+// const getCache = (req, res) => {
+//   client.get(req.params.id, (err, result) => {
+//     if (result) {
+//       res.send(result);
+//     } else {
+//       db.fetchFourRandomPAVsFromDB(req.params.id)
+//         .then(pavs => {
+//           client.setex(req.params.id, 60, JSON.stringify(pavs));
+//           res.send(pavs)
+//         });
+//     }
+//   })
+// }
+// app.get('/api/pavs/:id', getCache);
+
 app.use(
-  '/products/:itemId',
+  '/products/',
   proxy({
     target: 'http://localhost:3001',
     changeOrigin: true,
@@ -25,7 +55,7 @@ app.use(
 );
 
 app.use(
-  '/reviews/:itemId',
+  '/reviews',
   proxy({
     target: 'http://localhost:3003',
     changeOrigin: true,
@@ -33,7 +63,7 @@ app.use(
 );
 
 app.use(
-  '/api/pavs/:id',
+  '/post',
   proxy({
     target: 'http://localhost:8081',
     changeOrigin: true,
