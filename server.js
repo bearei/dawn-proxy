82a4a1a0ac56b982f8ea1b53566bb6d2bf77a500
Dawn <dawnsaq@gmail.com>
2019-03-13 12:33:41 -0700

    newrelic commented out

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-require('newrelic');
+// require('newrelic');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
